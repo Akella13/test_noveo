e2930ba401f2658b097783c79cff611e04225cbe
Arman Kozhakhmetov <kozhakhmetov13@gmail.com>
2021-04-01 13:49:39 +0300

    feat: axios init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,23 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      dogsList: {},
+    };
+  },
+  mounted() {
+    axios.get('https://dog.ceo/api/breeds/list/all/random/20')
+      .then(response => {
+        this.dogsList = response.data.message;
+      })
+      .catch(error => {
+        console.error(error.response);
+      });
+  },
 }
 </script>
