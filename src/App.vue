0af140b3b8182b5f4b4bfde21286bc711985aab8
Arman Kozhakhmetov <kozhakhmetov13@gmail.com>
2021-04-01 11:34:35 +0300

    feat: header init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <header>
+      <router-link to="/">Home</router-link>
+      <label>
+        Dog breed
+        <select>
+          <option>Husky</option>
+          <option>Bulldog</option>
+        </select>
+      </label>
+      <router-link to="/favourites">Favourites</router-link>
+    </header>
     <router-view/>
   </div>
 </template>
